1718d1fc70b07051bd92b85254cc0a7e0f6adc5f
ManeaAlex23 <48095398+ManeaAlex23@users.noreply.github.com>
2020-11-06 02:23:49 +0200

    Login and register page created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -13,7 +13,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router:Router,
   ) {
     this.initializeApp();
   }
@@ -22,6 +23,8 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.router.navigate(['login']);
+
     });
   }
 }
